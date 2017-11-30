81ea1ac5dc84f56d78d010134ee97b91b82848a0
Brittany Morris <brittmmendez@gmail.com>
2017-11-30 18:06:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,6 @@ katz_deli = []
 def line(array)
  if array.size==0
    puts "The line is currently empty."
- else
-   array.each_with_index do |name,index|
-     line_num=index+1
-     "Welcome, #{name}. You are number #{line_num} in line."
-   end
  end
 end
 
@@ -15,6 +10,7 @@ def take_a_number(name,katz_deli)
 end
 
 def now_serving(katz_deli)
+line
 puts "Now serving: #{katz_deli[0]}"
 katz_deli.shift
 end
