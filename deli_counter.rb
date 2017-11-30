9e94354232832353e308b191d874d50ac37da65b
Brittany Morris <brittmmendez@gmail.com>
2017-11-30 18:09:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,8 @@
 katz_deli = []
+
 def line(array)
+  katz_deli = []
  if array.size==0
    puts "The line is currently empty."
- else
-   puts current_line
-
  end
-end
-
-def take_a_number(name,katz_deli)
-
-end
-
-def now_serving(katz_deli)
-line
-puts "Now serving: #{katz_deli[0]}"
-katz_deli.shift
 end
